2cbdc2bc71947bea4d8be848c717ec1027085741
James Cook <jmsck55@gmail.com>
2020-08-15 01:28:58 -0400

    Add files via upload

--- test.ex
+++ test.ex
@@ -8,7 +8,7 @@ integer tmpAdjustRound
 
 trace(1)
 
-a = ToEun(2)
+a = ToEun("2")
 b = EunSqrt(a)
 b = b[2]
 c = EunMultiply(b,b)
@@ -28,6 +28,19 @@ adjustRound = tmpAdjustRound
 ? EunCompare(RemoveLastDigits(b), b)
 ? GetEqualLength()
 
-? GetMoreAccurate(ToEun("2.0004"), ToEun("1.998"))
+function myfunc(Eun n1)
+	sequence s
+	s = EunSqrt(n1)
+	return s[2]
+end function
+
+a = ToEun(1/3)
+a[3] += 10
+? EunSqrt(a)
+a[3] -= 10
+
+? EunSqrt(a)
+? GetMoreAccurateFunc(routine_id("myfunc"), a)
+
 
 --End-of-file.
